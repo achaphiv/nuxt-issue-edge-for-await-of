fec6f69fff3d9e9dbe9922c1ec92763dab88f197
Alex Chaphiv <alex.chaphiv@whitehawk.com>
2019-11-15 14:25:03 -0500

    feat: use `for await of`

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,32 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt-issue-edge-for-await-of
-      </h1>
-      <h2 class="subtitle">
-        My first-rate Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    Showing elements of async iterator:
+    <ul>
+      <li v-for="value in values" :key="value">
+        {{ value }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  data() {
+    return {
+      values: []
+    };
+  },
+  async created() {
+    for await (const value of this.asyncIterable()) {
+      this.values.push(value);
+    }
+  },
+  methods: {
+    async *asyncIterable() {
+      for (let i = 0; i < 10; i++) {
+        yield Promise.resolve(i);
+      }
+    }
   }
-}
+};
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
